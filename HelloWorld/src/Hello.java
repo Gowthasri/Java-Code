a965039cc1f3273ecbbb9accd0d6d5bda3c8c7a5
Gowthasri Nallamilli <gowthasri.reddynallamilli@contractor.ca.com>
2018-02-15 15:54:53 -0600

    maste: adding Hello.class

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args){
+
+        System.out.println("Hello Gowthasri!!");
+        double var1=20;
+        double var2=80;
+        double total=(var1+var2)*25;
+        double rem=(var1+var1)%40;
+        if(rem<=20){
+            System.out.println("Total was wover the limit");
+        }
+
+    }
+}
